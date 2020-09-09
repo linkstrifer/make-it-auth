e3e72fcc4cdce0e2e112442522f191384111acce
Link Strifer <linkstrifer@outlook.com>
2020-09-08 20:10:48 -0500

    Saludame 2

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const express = require("express");
 const app = express();
 
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 app.get("/", (req, res) => {
   const { nombre } = req.query;
   let template = "Hola ";
 
   if (nombre) {
-    template += nombre;
+    template += capitalize(nombre);
   } else {
     template += "desconocido";
   }
 
   template += "!";
+
+  res.send(`<h1>${template}</h1>`);
+});
+
+app.get("/makers/:nombre", (req, res) => {
+  const { nombre } = req.params;
+  let template = "Hola ";
+
+  template += capitalize(nombre) + "!";
 
   res.send(`<h1>${template}</h1>`);
 });
