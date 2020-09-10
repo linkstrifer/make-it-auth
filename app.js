9d2ffbcc7d1f154521983d3f4c83023e36435e7e
Link Strifer <linkstrifer@outlook.com>
2020-09-10 18:22:04 -0500

    cabeceras

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
 const express = require("express");
 const app = express();
 
-function capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-app.use(express.urlencoded({ extended: true }));
-
 app.get("/", function (req, res) {
-  res.send(`<form action="/" method="post">
-    <input type="text" name="nombre" />
-    <button type="submit">Enviar</button>
-  </form>`);
-});
+  const userAgent = req.headers["user-agent"];
 
-app.post("/", function (req, res) {
-  const { nombre } = req.body;
-
-  res.send(`<h1>Hola ${capitalize(nombre)}!</h1>`);
-});
-
-app.get("/makers/:nombre", (req, res) => {
-  const { nombre } = req.params;
-
-  res.send(`<h1>Hola ${capitalize(nombre)}!</h1>`);
+  res.send(userAgent);
 });
 
 app.listen(3000, () => {
